d1786589c562a135bbca61a71c2bc7d54e586771
zpahf2018 <zpahf2018@naver.com>
2026-02-12 16:15:09 +0900

    feat : Initial project setup with Spring Boot and Docker

---
+++ backend/src/test/java/io/github/taejun/matching/MatchingEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.taejun.matching;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatchingEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
